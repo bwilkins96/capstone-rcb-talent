c6828005b7f05533eb02c5a541852f1793dd4f49
Cristian Astorga <Cristian.astorga235@gmail.com>
2024-05-07 17:02:43 -0500

    Updated began testing Interview repo
    Bug Found: tests will pass once after the test schema is run but fail if the test suit is ran again. Not sure the cause

--- server/app-tracker/src/test/java/learn/app_tracker/data/InterviewJdbcTemplateRepositoryTest.java
+++ server/app-tracker/src/test/java/learn/app_tracker/data/InterviewJdbcTemplateRepositoryTest.java
@@ -1,7 +1,85 @@
 package learn.app_tracker.data;
 
+import learn.app_tracker.models.Interview;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class InterviewJdbcTemplateRepositoryTest {
 
+    @Autowired
+    InterviewJdbcTemplateRepository repository;
+
+    @Autowired
+    KnownGoodState knownGoodState;
+
+    @BeforeEach
+    void setup() {
+        knownGoodState.set();
+    }
+
+    @Test
+    void shouldFindAll() throws DataException {
+        List<Interview> interviews = repository.findAll();
+        assertNotNull(interviews);
+
+        assertTrue(interviews.size() >= 3 && interviews.size() <= 5);
+    }
+
+    @Test
+    void shouldFindAllByApplicationId() throws DataException {
+        List<Interview> interviews = repository.findAllByApplicationId(2);
+        assertNotNull(interviews);
+
+        assertEquals(1, interviews.size());
+    }
+
+    @Test
+    void shouldNotFindAnyOfMissingApplication() throws  DataException {
+        List<Interview> interviews = repository.findAllByApplicationId(55);
+        assertEquals(0, interviews.size());
+    }
+
+    @Test
+    void shouldFindByExistingId() throws DataException {
+        Interview interview = repository.findById(2);
+        assertNotNull(interview);
+    }
+
+    @Test
+    void shouldNotFindMissingID() throws DataException {
+        Interview interview = repository.findById(24);
+        assertNull(interview);
+    }
+
+    @Test
+    void shouldAddInterview() throws DataException {
+
+    }
+
+    @Test
+    void shouldUpdateExistingInterview() throws DataException {
+
+    }
+
+    @Test
+    void shouldNotUpdateMissingInterview() throws DataException {
+
+    }
+
+    @Test
+    void shouldDeleteExistingInterview() throws DataException {
+
+    }
+
+    @Test
+    void shouldNotDeleteMissingInterview() throws DataException {
+
+    }
 }
