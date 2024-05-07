fc7d016af6e669f7b9d7833ec8a25e20771467af
Benjamin Wilkins <104269697+bwilkins96@users.noreply.github.com>
2024-05-07 15:31:22 -0400

    company repo test - set up

---
+++ server/app-tracker/src/test/java/learn/app_tracker/data/CompanyJdbcTemplateRepositoryTest.java
@@ -0,0 +1,24 @@
+package learn.app_tracker.data;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CompanyJdbcTemplateRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void shouldFindAll() {
+
+    }
+
+    @Test
+    void shouldFindById() {
+
+    }
+
+}
