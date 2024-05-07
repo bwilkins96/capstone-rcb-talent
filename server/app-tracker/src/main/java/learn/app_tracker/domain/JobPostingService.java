30bad85b07116901b13bea61c53886144c453ce8
Benjamin Wilkins <104269697+bwilkins96@users.noreply.github.com>
2024-05-07 19:25:21 -0400

    job posting service

---
+++ server/app-tracker/src/main/java/learn/app_tracker/domain/JobPostingService.java
@@ -0,0 +1,26 @@
+package learn.app_tracker.domain;
+
+import learn.app_tracker.data.JobPostingRepository;
+import learn.app_tracker.models.JobPosting;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class JobPostingService {
+
+    private final JobPostingRepository repository;
+
+    public JobPostingService(JobPostingRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<JobPosting> findAll() {
+        return repository.findAll();
+    }
+
+    JobPosting findById(int postingId) {
+        return repository.findById(postingId);
+    };
+
+}
