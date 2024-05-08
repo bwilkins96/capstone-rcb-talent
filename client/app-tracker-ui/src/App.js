9488bacbdff10f460c4dbac3b49f01db1bcea096
rasinha <rasinha@ucsc.edu>
2024-05-08 10:13:37 -0700

    set up user interface

--- client/app-tracker-ui/src/App.js
+++ client/app-tracker-ui/src/App.js
@@ -1,11 +1,31 @@
 import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import JobApplicationForm from './components/JobApplicationForm';
+import JobApplicationList from './components/JobApplicationList';
+import About from './components/About';
+import NotFound from './components/NotFound';
+import Visualization from './components/Visualization';
+
 
 function App() {
   return (
-    <>
-    <h1>Hello from RCB Talent!</h1>
-    </>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='application/add' element={<JobApplicationForm />} />
+        <Route path='/application/edit/:id' element={<JobApplicationForm />} />
+        <Route path='/applications' element={<JobApplicationList />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sankey/diagram' element={<Visualization />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </Router >
   );
 }
 
 export default App;
+
---
+++ client/app-tracker-ui/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+    return (<>
+        <nav>
+            <Link to={'/'}>Home</Link>
+            <Link to={`/about`}>About</Link>
+            <Link to={`/applications`}>Applications</Link>
+            <Link to={`/sankey/diagram`}>Sankey Diagram</Link>
+        </nav>
+    </>);
+
+}
+
+export default Navbar;
+
