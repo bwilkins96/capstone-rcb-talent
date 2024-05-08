10c3a64d5c3339873337c16a0ae7b2f122e98c8f
Benjamin Wilkins <104269697+bwilkins96@users.noreply.github.com>
2024-05-07 21:01:58 -0400

    added annotation to application repo test

--- server/app-tracker/src/test/java/learn/app_tracker/data/JobApplicationJdbcTemplateRepositoryTest.java
+++ server/app-tracker/src/test/java/learn/app_tracker/data/JobApplicationJdbcTemplateRepositoryTest.java
@@ -3,9 +3,11 @@ package learn.app_tracker.data;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class JobApplicationJdbcTemplateRepositoryTest {
 
     @Autowired
---
+++ server/app-tracker/src/test/java/learn/app_tracker/domain/JobApplicationServiceTest.java
@@ -0,0 +1,7 @@
+package learn.app_tracker.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JobApplicationServiceTest {
+
+}
