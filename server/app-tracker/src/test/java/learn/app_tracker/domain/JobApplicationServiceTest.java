6523e1f7b297d7d9d2f71950c90a9424b31c6469
Benjamin Wilkins <104269697+bwilkins96@users.noreply.github.com>
2024-05-08 12:50:10 -0400

    application service tests - set up methods

--- server/app-tracker/src/test/java/learn/app_tracker/domain/JobApplicationServiceTest.java
+++ server/app-tracker/src/test/java/learn/app_tracker/domain/JobApplicationServiceTest.java
@@ -1,6 +1,8 @@
 package learn.app_tracker.domain;
 
+import learn.app_tracker.data.InterviewRepository;
 import learn.app_tracker.data.JobApplicationRepository;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -16,5 +18,68 @@ class JobApplicationServiceTest {
 
     @MockBean
     JobApplicationRepository repository;
+
+    @MockBean
+    InterviewRepository interviewRepository;
+
+    @Test
+    void shouldAdd() {
+
+    }
+
+    @Test
+    void shouldNotAddNull() {
+
+    }
+
+    @Test
+    void shouldNotAddMissingPostingId() {
+
+    }
+
+    @Test
+    void shouldNotAddNullStatus() {
+
+    }
+
+    @Test
+    void shouldNotAddNullDateApplied() {
+
+    }
+
+    @Test
+    void shouldUpdate() {
+
+    }
+
+    @Test
+    void shouldNotUpdateNull() {
+
+    }
+
+    @Test
+    void shouldNotUpdateMissingId() {
+
+    }
+
+    @Test
+    void shouldNotUpdateMissingPostingId() {
+
+    }
+
+    @Test
+    void shouldNotUpdateNullStatus() {
+
+    }
+
+    @Test
+    void shouldNotUpdateNullOrigin() {
+
+    }
+
+    @Test
+    void shouldNotUpdateNullDateApplied() {
+
+    }
 
 }
