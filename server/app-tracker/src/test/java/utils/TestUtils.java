b2a4d9d7c7106d762466383998327e8ce221e753
Benjamin Wilkins <104269697+bwilkins96@users.noreply.github.com>
2024-05-08 10:50:41 -0400

    application repo tests - set up test data

--- server/app-tracker/src/test/java/utils/TestUtils.java
+++ server/app-tracker/src/test/java/utils/TestUtils.java
@@ -1,8 +1,12 @@
 package utils;
 
 import learn.app_tracker.models.Company;
+import learn.app_tracker.models.JobApplication;
 import learn.app_tracker.models.JobPosting;
+import learn.app_tracker.models.enums.Origin;
+import learn.app_tracker.models.enums.Status;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -47,5 +51,29 @@ public class TestUtils {
 
         return posting;
     }
+
+    public static JobApplication getTestApplication() {
+        JobApplication application = new JobApplication();
+
+        application.setStatus(Status.PENDING);
+        application.setOrigin(Origin.COLD_APPLY);
+        application.setDateApplied(LocalDate.of(2023, 1, 20));
+        application.setNotes("I wonder what will happen");
+
+        JobPosting posting = new JobPosting();
+        posting.setPostingId(3);
+        application.setPosting(posting);
+
+        return application;
+    }
+
+    public static JobApplication getTestApplicationFull() {
+        JobApplication application = getTestApplication();
+        application.setPosting(getTestPostingFull());
+        return application;
+    }
+
+    // (posting_id, status_id, origin_id, date_applied, notes)
+    // (3, 1, 1, '2024-01-20', 'I wonder what will happen')
 
 }
