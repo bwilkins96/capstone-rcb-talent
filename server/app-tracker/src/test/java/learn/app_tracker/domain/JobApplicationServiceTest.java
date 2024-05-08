6a784b35fdba098a223ea579bf768ba60ab2eed5
Benjamin Wilkins <104269697+bwilkins96@users.noreply.github.com>
2024-05-07 21:03:16 -0400

    application service test set up

--- server/app-tracker/src/test/java/learn/app_tracker/domain/JobApplicationServiceTest.java
+++ server/app-tracker/src/test/java/learn/app_tracker/domain/JobApplicationServiceTest.java
@@ -1,7 +1,20 @@
 package learn.app_tracker.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
+import learn.app_tracker.data.JobApplicationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class JobApplicationServiceTest {
+
+    @Autowired
+    JobApplicationService service;
+
+    @MockBean
+    JobApplicationRepository repository;
 
 }
